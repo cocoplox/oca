2f50af2bda4e1866614e1b2049f51782878fe403
cocoplox <ivanvegasantaella5@gmail.com>
2023-12-19 12:17:28 +0100

    Inicio oca

---
+++ oca.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.random.*;
+public class oca{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        input.close();
+    }
+}
