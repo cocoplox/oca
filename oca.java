8f494bfe29d0705674c3f856603caac0e3b297ef
cocoplox <ivanvegasantaella5@gmail.com>
2023-12-19 12:52:58 +0100

    Bucle test branch bucletest

--- oca.java
+++ oca.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 import java.util.random.*;
 public class oca{
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        
-        input.close();
+        int[] arrayPrueba = new int[]{1,2,3,4,5};
+        for(int i = 0; i < arrayPrueba.length;i++){
+            System.out.println("Valor: " + arrayPrueba[i]);
+        }
     }
 }
