6670672850e24cf22b4fa361da893e298e2c9775
Ivan casa <ivanvegasantaella@hotmail.es>
2023-12-22 00:32:36 +0100

    Añadido invariable GAME

--- oca.java
+++ oca.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.random.*;
 public class oca{
+    public static boolean GAME = true;
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         
